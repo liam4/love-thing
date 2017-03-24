2d8790ca154881be8c87c70a186209ca399c2164
liam4 <towerofnix@gmail.com>
2017-03-23 23:38:30 -0300

    Actor label transition

--- dialog.lua
+++ dialog.lua
@@ -11,8 +11,12 @@ function Dialog:init (self)
   self.index = 0
   self.isDone = true
   self.actor = nil
-
   self.color = { 128, 128, 128 }
+
+  self.transitionActor = nil
+  self.transitionActorAnim = 1
+
+  self.lastActor = nil
 end
 
 function Dialog:setText (self, text)
@@ -22,8 +26,21 @@ function Dialog:setText (self, text)
   self.isDone = false
 end
 
+-- [nil -> Median]
+-- [Median -> Beleth]
+
+-- nil == median? nil -> median
+-- median == median?
+-- beleth == median? median -> beleth
+
 function Dialog:setActor (self, actor)
-  self.actor = actor
+  if not (actor == self.lastActor) then
+    self.transitionActorAnim = 0
+    self.oldActor = self.actor
+
+    self.lastActor = actor
+    self.actor = actor
+  end
 end
 
 function Dialog:update (self)
@@ -62,11 +79,12 @@ function Dialog:draw (self)
   love.graphics.setFont(speechFont)
   love.graphics.print(self.displayText, 40, top + 40)
 
-  if self.actor then
+  function drawActorLabel(actor, transition)
     local labelWidth = 240
     local labelHeight = 40
     local labelTop = top - labelHeight
     local labelLeft = 0
+    local labelDrawTop = labelTop + labelHeight * transition
 
     love.graphics.stencil(function ()
       love.graphics.rectangle("fill",
@@ -78,13 +96,30 @@ function Dialog:draw (self)
     local color = self.actor.color
     love.graphics.setColor(color[1], color[2], color[3], 200)
     love.graphics.rectangle("fill",
-      labelLeft, labelTop, labelWidth, labelHeight)
+      labelLeft, labelDrawTop, labelWidth, labelHeight)
 
     love.graphics.setColor(255, 255, 255)
     love.graphics.setFont(labelFont)
-    love.graphics.print(self.actor.name, labelLeft + 20, top - 33)
+    love.graphics.print(self.actor.name, labelLeft + 20, labelDrawTop + 7)
 
     love.graphics.setStencilTest()
+  end
+
+  if self.oldActor then
+    drawActorLabel(self.oldActor, self.transitionActorAnim)
+  end
+
+  if self.actor then
+    drawActorLabel(self.actor, 1 - self.transitionActorAnim)
+  end
+
+  if self.transitionActorAnim < 1 then
+    self.transitionActorAnim = self.transitionActorAnim + 0.25 * (
+      1 - self.transitionActorAnim)
+
+    if self.transitionActorAnim >= 1 then
+      self.transitionActorAnim = 1
+    end
   end
 end
 
